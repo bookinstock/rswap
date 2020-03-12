c84f852f4e225297ec10d11fafe2be63f0d40edf
Wende Lu <wendelu1991@gmail.com>
2020-03-12 10:30:08 +0800

    swap examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    fs::rename("examples/a.txt", "examples/c.txt")?; // Rename a.txt to b.txt
+    fs::rename("examples/b.txt", "examples/a.txt")?;
+    fs::rename("examples/c.txt", "examples/b.txt")?;
+    Ok(())
 }
