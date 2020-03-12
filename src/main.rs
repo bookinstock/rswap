466b39498658c8709838a1125332298139a510be
Wende Lu <wendelu1991@gmail.com>
2020-03-12 10:37:06 +0800

    swap function complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::env;
+// use std::io;
 use std::fs;
 
+
 fn main() -> std::io::Result<()> {
-    fs::rename("examples/a.txt", "examples/c.txt")?; // Rename a.txt to b.txt
-    fs::rename("examples/b.txt", "examples/a.txt")?;
-    fs::rename("examples/c.txt", "examples/b.txt")?;
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let file_name_1 = &args[1];
+    let file_name_2 = &args[2];
+    let file_name_tmp = &format!("{}_tmp", file_name_1)[..];
+
+    fs::rename(file_name_1, file_name_tmp)?; // Rename a.txt to b.txt
+    fs::rename(file_name_2, file_name_1)?;
+    fs::rename(file_name_tmp, file_name_2)?;
     Ok(())
 }
